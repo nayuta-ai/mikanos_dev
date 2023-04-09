64193ac5f979094a601055adf62c8434f2f00ee9
nayuta-ai <nayuta723@gmail.com>
2023-04-09 08:43:02 +0000

    Count the number of interruptions

--- kernel/interrupt.cpp
+++ kernel/interrupt.cpp
@@ -8,6 +8,7 @@
 
 #include "asmfunc.h"
 #include "segment.hpp"
+#include "timer.hpp"
 
 std::array<InterruptDescriptor, 256> idt;
 
@@ -34,7 +35,7 @@ __attribute__((interrupt)) void IntHandlerXHCI(InterruptFrame* frame) {
 }
 
 __attribute__((interrupt)) void IntHandlerLAPICTimer(InterruptFrame* frame) {
-  msg_queue->push_back(Message{Message::kInterruptLAPICTimer});
+  LAPICTimerOnInterrupt();
   NotifyEndOfInterrupt();
 }
 }  // namespace
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -93,11 +93,13 @@ extern "C" void KernelMainNewStack(
   InitializeLAPICTimer();
 
   char str[128];
-  unsigned int count = 0;
 
   while (true) {
-    ++count;
-    sprintf(str, "%010u", count);
+    __asm__("cli");
+    const auto tick = timer_manager->CurrentTick();
+    __asm__("sti");
+
+    sprintf(str, "%010lu", tick);
     FillRectangle(*main_window->Writer(), {24, 28}, {8 * 10, 16},
                   {0xc6, 0xc6, 0xc6});
     WriteString(*main_window->Writer(), {24, 28}, str, {0, 0, 0});
@@ -105,7 +107,7 @@ extern "C" void KernelMainNewStack(
 
     __asm__("cli");
     if (main_queue->size() == 0) {
-      __asm__("sti");
+      __asm__("sti\n\thlt");
       continue;
     }
 
