3342a80a3299c37a5e24bd589b1df1699380ce34
nayuta-ai <nayuta723@gmail.com>
2023-04-17 16:53:58 +0000

    Add command history

--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -17,6 +17,7 @@ Terminal::Terminal() {
   layer_id_ =
       layer_manager->NewLayer().SetWindow(window_).SetDraggable(true).ID();
   Print(">");
+  cmd_history_.resize(8);
 }
 
 Rectangle<int> Terminal::BlinkCursor() {
@@ -34,7 +35,12 @@ Rectangle<int> Terminal::InputKey(uint8_t modifier, uint8_t keycode,
 
   if (ascii == '\n') {
     linebuf_[linebuf_index_] = 0;
+    if (linebuf_index_ > 0) {
+      cmd_history_.pop_back();
+      cmd_history_.push_front(linebuf_);
+    }
     linebuf_index_ = 0;
+    cmd_history_index_ = -1;
     cursor_.x = 0;
     if (cursor_.y < kRows - 1) {
       ++cursor_.y;
@@ -62,6 +68,10 @@ Rectangle<int> Terminal::InputKey(uint8_t modifier, uint8_t keycode,
       WriteAscii(*window_->Writer(), CalcCursorPos(), ascii, {255, 255, 255});
       ++cursor_.x;
     }
+  } else if (keycode == 0x51) {  // down arrow
+    draw_area = HistoryUpDown(-1);
+  } else if (keycode == 0x52) {  // up arrow
+    draw_area = HistoryUpDown(1);
   }
 
   DrawCursor(true);
@@ -148,6 +158,32 @@ void Terminal::ExecuteLine() {
     Print(command);
     Print("\n");
   }
+}
+
+Rectangle<int> Terminal::HistoryUpDown(int direction) {
+  if (direction == -1 && cmd_history_index_ >= 0) {
+    --cmd_history_index_;
+  } else if (direction == 1 && cmd_history_index_ + 1 < cmd_history_.size()) {
+    ++cmd_history_index_;
+  }
+
+  cursor_.x = 1;
+  const auto first_pos = CalcCursorPos();
+
+  Rectangle<int> draw_area{first_pos, {8 * (kColumns - 1), 16}};
+  FillRectangle(*window_->Writer(), draw_area.pos, draw_area.size, {0, 0, 0});
+
+  const char* history = "";
+  if (cmd_history_index_ >= 0) {
+    history = &cmd_history_[cmd_history_index_][0];
+  }
+
+  strcpy(&linebuf_[0], history);
+  linebuf_index_ = strlen(history);
+
+  WriteString(*window_->Writer(), first_pos, history, {255, 255, 255});
+  cursor_.x = linebuf_index_ + 1;
+  return draw_area;
 }
 
 void TaskTerminal(uint64_t task_id, int64_t data) {
