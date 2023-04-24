c2b45afd92e6fcb4083c73beb447c3e371ad9b66
nayuta-ai <nayuta723@gmail.com>
2023-04-24 13:10:57 +0000

    Add winhello application which can open window and write string

--- apps/syscall.h
+++ apps/syscall.h
@@ -1,4 +1,6 @@
+#include <cstddef>
 #include <cstdint>
+
 #include "../kernel/logger.hpp"
 
 extern "C" {
@@ -11,5 +13,8 @@ struct SyscallResult {
 SyscallResult SyscallLogString(LogLevel level, const char* message);
 SyscallResult SyscallPutString(int fd, const char* s, size_t len);
 void SyscallExit(int exit_code);
+SyscallResult SyscallOpenWindow(int w, int h, int x, int y, const char* title);
+SyscallResult SyscallWinWriteString(unsigned int layer_id, int x, int y, uint32_t color, const char* s);
 
-} // extern "C"
+
+}  // extern "C"
