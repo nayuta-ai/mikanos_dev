322c08eb63df76f17e92663553ad24a8cf8682b3
nayuta-ai <nayuta723@gmail.com>
2023-04-24 06:40:51 +0000

    Add syscall function

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -29,6 +29,7 @@
 #include "paging.hpp"
 #include "pci.hpp"
 #include "segment.hpp"
+#include "syscall.hpp"
 #include "task.hpp"
 #include "terminal.hpp"
 #include "timer.hpp"
@@ -149,6 +150,8 @@ extern "C" void KernelMainNewStack(
   const int kTimer05Sec = static_cast<int>(kTimerFreq * 0.5);
   timer_manager->AddTimer(Timer{kTimer05Sec, kTextboxCursorTimer});
   bool textbox_cursor_visible = false;
+
+  InitializeSyscall();
 
   InitializeTask();
   Task& main_task = task_manager->CurrentTask();
