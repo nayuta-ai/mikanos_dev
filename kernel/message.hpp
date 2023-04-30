25f679a25ea9ae957ab763b953588b1b3c4463b5
nayuta-ai <nayuta723@gmail.com>
2023-04-30 06:12:15 +0000

    Add paint application

--- kernel/message.hpp
+++ kernel/message.hpp
@@ -10,6 +10,7 @@ struct Message {
     kLayer,
     kLayerFinish,
     kMouseMove,
+    kMouseButton,
   } type;
 
   uint64_t src_task;
@@ -38,5 +39,11 @@ struct Message {
       int dx, dy;
       uint8_t buttons;
     } mouse_move;
+
+    struct {
+      int x, y;
+      int press;  // 1: press, 0: release
+      int button;
+    } mouse_button;
   } arg;
 };
