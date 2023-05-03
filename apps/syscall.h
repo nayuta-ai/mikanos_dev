b9d8fc72c063e30c9d1521b4eb942e31a2c34978
nayuta-ai <nayuta723@gmail.com>
2023-05-03 08:46:28 +0000

    Add function for demand paging

--- apps/syscall.h
+++ apps/syscall.h
@@ -41,6 +41,8 @@ struct SyscallResult SyscallCreateTimer(unsigned int type, int timer_value,
                                         unsigned long timeout_ms);
 struct SyscallResult SyscallOpenFile(const char* path, int flags);
 struct SyscallResult SyscallReadFile(int fd, void* buf, size_t count);
+struct SyscallResult SyscallDemandPages(size_t num_pages, int flags);
+
 #ifdef __cplusplus
 }  // extern "C"
 #endif
