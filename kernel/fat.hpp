f789a00ec43ee8793eb0a6981ec417cd64eeddbe
nayuta-ai <nayuta723@gmail.com>
2023-05-03 09:46:26 +0000

    Add function for Memory Maped File

--- kernel/fat.hpp
+++ kernel/fat.hpp
@@ -148,7 +148,7 @@ bool NameIsEqual(const DirectoryEntry& entry, const char* name);
  * @param entry Directory entry representing the file
  * @return Number of bytes read.
  */
-size_t LoadFile(void* buf, size_t len, const DirectoryEntry& entry);
+size_t LoadFile(void* buf, size_t len, DirectoryEntry& entry);
 
 bool IsEndOfClusterchain(unsigned long cluster);
 
@@ -199,6 +199,8 @@ class FileDescriptor : public ::FileDescriptor {
   explicit FileDescriptor(DirectoryEntry& fat_entry);
   size_t Read(void* buf, size_t len) override;
   size_t Write(const void* buf, size_t len) override;
+  size_t Size() const override { return fat_entry_.file_size; }
+  size_t Load(void* buf, size_t len, size_t offset) override;
 
  private:
   DirectoryEntry& fat_entry_;
